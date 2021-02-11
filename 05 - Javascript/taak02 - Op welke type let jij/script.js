64cab7e22e67122a69b6bb1d5a4825e058160a81
Meraichio <meraichio.sijp@student.rocva.nl>
2021-02-11 11:59:52 +0100

    datatypes

---
+++ 05 - Javascript/taak02 - Op welke type let jij/script.js
@@ -0,0 +1,20 @@
+let mijnVoornaam = "Meraichio"
+console.log(mijnVoornaam);
+
+let mijnAchternaam = 'Sijp'
+console.log(mijnAchternaam);
+
+let leeftijd = 17;
+console.log(leeftijd);
+
+let ikBenStemGerechtig = false;
+console.log(ikBenStemGerechtig);
+
+let ikMagLegaalAlcoholKopen = false;
+console.log(ikMagLegaalAlcoholKopen);
+
+let ikMagBrommerRijden = true;
+console.log(ikMagBrommerRijden);
+
+let SoftwareOntwikkeling = null;
+console.log(SoftwareOntwikkeling);
