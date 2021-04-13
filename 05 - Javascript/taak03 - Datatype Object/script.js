ff9c3e5777c94801cc03a722ecbf13d7b2a142da
Meraichio <meraichio.sijp@student.rocva.nl>
2021-04-13 10:13:20 +0200

    console log

---
+++ 05 - Javascript/taak03 - Datatype Object/script.js
@@ -0,0 +1,12 @@
+let student = {
+    voornaam: 'Meraichio',
+    achternaam: 'Sijp',
+    leeftijd: 18,
+    ikBenStemGerechtig: true,
+    ikMagLegaalAlcoholKopen: true,
+    ikMagBrommerRijden: true,
+   
+}
+console.log (student);
+
+console.log(student.leeftijd);
---
+++ 05 - Javascript/taak04 - String kan plakken/script.js
@@ -0,0 +1,7 @@
+let mijnVoornaam = 'Meraichio';
+let mijnAchternaam= 'Sijp';
+let mijnLeeftijd=  18;
+
+alert(mijnVoornaam + mijnAchternaam);
+
+console.log('Mijn naam is <voornaam> <achternaam> en mijn leeftijd is <leeftijd> jaar');
