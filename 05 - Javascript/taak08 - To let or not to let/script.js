0a6b5e2c5497ff6eb5ede672557f73e8258554cd
Meraichio <meraichio.sijp@student.rocva.nl>
2021-04-15 10:54:13 +0200

    To let or not to let

---
+++ 05 - Javascript/taak08 - To let or not to let/script.js
@@ -0,0 +1,15 @@
+let codingIsCool;
+codingIsCool = true;
+codingIsCool = false;
+console.log (codingIsCool);
+
+let huidigJaartal;
+huidigJaartal = 2021;
+
+let geboorteJaar;
+geboorteJaar = 2003; 
+
+let mijnLeeftijd;
+mijnLeeftijd = huidigJaartal - geboorteJaar;
+
+console.log(mijnLeeftijd)
