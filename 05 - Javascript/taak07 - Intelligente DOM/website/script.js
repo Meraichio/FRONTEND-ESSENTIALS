36c84a754648605401ff868dd52a4d0b9aa8ca11
Meraichio <meraichio.sijp@student.rocva.nl>
2021-04-15 10:37:44 +0200

    intelligente DOM

--- 05 - Javascript/taak07 - Intelligente DOM/website/script.js
+++ 05 - Javascript/taak07 - Intelligente DOM/website/script.js
@@ -1,9 +1,16 @@
-
+document.title = "Dit zijn drie films";
 console.log(window.document);
 
+console.log(document.body.innerHTML);
 
+document.querySelector("#posterimg1").src = "index.html";
+var elem = document.querySelector('#posterimg1');
+elem.style.backgroundColor = 'purple';
 
-// document.querySelector("#posterimg1").src = null;
+document.querySelector("#posterimg2").src = "index.html";
+var elem = document.querySelector('#posterimg2');
+elem.style.backgroundColor = 'green';
 
-
-// document.querySelector("#poster1");
+document.querySelector("#posterimg3").src = "index.html";
+var elem = document.querySelector('#posterimg3');
+elem.style.backgroundColor = 'orange';
