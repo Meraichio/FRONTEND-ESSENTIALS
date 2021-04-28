438112238dc2e1119d5ecebfdfbcf7326490a866
Meraichio <meraichio.sijp@student.rocva.nl>
2021-04-28 14:35:52 +0200

    vlaggen website

---
+++ opdracht/eindopdracht/script.js
@@ -0,0 +1,9 @@
+let bezoeker = {
+    voornaam : prompt("Wat is je naam?"),
+    achternaam : prompt("Wat is je achternaam?"),
+    Email : prompt('Wat is je Email'),
+    leeftijd : prompt("Wat is je leeftijd?"),
+    woonplaats : prompt('Wat is je woonplaats'),
+};
+
+console.log('')
